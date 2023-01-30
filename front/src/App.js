1b5d1caebd050d0b3f4d84a727192b8006177dd5
Ryunki Shin <ryunkishin@gmail.com>
2023-01-30 11:09:29 +0900

    added app.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
 import './App.css';
+
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div className="bg-store"> */}
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      {/* </div> */}
+      <Footer/>
+    </>
+  
   );
 }
 
